5506634245a42a3a8e6b9a7677019ae69ae4e29b
Finn Freiheit <finnfreiheit@MBP-von-Finn.fritz.box>
2022-02-08 16:17:41 +0100

    cors

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors()); 
 app.use('/', routes);
 
 // connect to mongoDB
