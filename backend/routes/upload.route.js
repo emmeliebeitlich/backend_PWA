6de78a50c126539bb1a36766f02794121ab6127f
emmeliebeitlich <72192664+emmeliebeitlich@users.noreply.github.com>
2022-05-03 12:23:39 +0200

    added middleware into backend

--- backend/routes/upload.route.js
+++ backend/routes/upload.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const upload = require('../../middleware/upload');
+const upload = require('../middleware/upload');
 const router = express.Router();
 
 router.post('/', upload.single('file'), (req, res) => {
